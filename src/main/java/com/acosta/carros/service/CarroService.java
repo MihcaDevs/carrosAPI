3acaacbc0f6221a2a301ec3cdd71ac4479a749ac
Alberto Costa <mihcadevs@gmail.com>
2019-09-25 23:25:10 -0300

    Atualizar o carro

--- src/main/java/com/acosta/carros/service/CarroService.java
+++ src/main/java/com/acosta/carros/service/CarroService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import com.acosta.carros.domain.Carro;
 import com.acosta.carros.repository.CarroRepository;
@@ -27,9 +28,28 @@ public class CarroService {
 		return rep.findByTipo(tipo);
 	}
 
-	public Carro save(Carro carro) {
+	public Carro insert(Carro carro) {
 		return rep.save(carro);		
 		
+	}
+	
+	public Carro update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possível atualizar o registro");
+		
+		Optional<Carro> optional = getCarroById(id);
+		if(optional.isPresent()) {
+			Carro db= optional.get();
+			
+			db.setNome(carro.getNome());
+			db.setTipo(carro.getTipo());
+			System.out.println("Carro id " + db.getId());
+			
+			rep.save(db);
+			
+			return db;
+		} else {
+			throw new RuntimeException("Não foi possivel atualizar o registro");
+		}
 	}
 	
 
