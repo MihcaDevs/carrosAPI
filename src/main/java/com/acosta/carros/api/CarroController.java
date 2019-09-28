c425708b0a4c7bfe72c7d6a0d9e5ef4b99e735f3
Alberto Costa <mihcadevs@gmail.com>
2019-09-28 20:41:44 -0300

    Implementando ResponseEntity no metodo get - lista

--- src/main/java/com/acosta/carros/api/CarroController.java
+++ src/main/java/com/acosta/carros/api/CarroController.java
@@ -3,6 +3,8 @@ package com.acosta.carros.api;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,14 +25,17 @@ public class CarroController {
 	private CarroService service;
 
 	@GetMapping()
-	public Iterable<Carro> get() {
-		return service.getCarros();
+	public ResponseEntity<Iterable<Carro>> get() {
+		return new ResponseEntity<>(service.getCarros(), HttpStatus.OK);
 
 	}
 
 	@GetMapping("/{id}")
-	public Optional<Carro> get(@PathVariable("id") Long id) {
-		return service.getCarroById(id);
+	public ResponseEntity get(@PathVariable("id") Long id) {
+		Optional<Carro> carro = service.getCarroById(id);
+		return carro
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 	@GetMapping("/tipo/{tipo}")
