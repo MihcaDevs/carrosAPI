4eeaae6ecd9106acddaaebfbe7c2129a7c23ad89
Alberto Costa <mihcadevs@gmail.com>
2019-10-02 22:50:50 -0300

    teste para inserir um carro

--- src/test/java/com/acosta/carros/CarrosApiApplicationTests.java
+++ src/test/java/com/acosta/carros/CarrosApiApplicationTests.java
@@ -2,12 +2,28 @@ package com.acosta.carros;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.acosta.carros.domain.Carro;
+import com.acosta.carros.service.CarroService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CarrosApiApplicationTests {
+	
+	@Autowired
+	private CarroService service;
+	
+	@Test
+	public void test1() {
+		Carro carro = new Carro();
+		carro.setNome("ferrari");
+		carro.setTipo("esportivos");
+		
+		service.insert(carro);
+	}
 
 	@Test
 	public void contextLoads() {
