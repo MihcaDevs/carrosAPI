a615f1c956991adf75b6712e66ec6167d6777d0a
Alberto Costa <mihcadevs@gmail.com>
2019-10-06 00:17:16 -0300

    Teste para inserir e deletar um  carro

--- src/test/java/com/acosta/carros/CarrosApiApplicationTests.java
+++ src/test/java/com/acosta/carros/CarrosApiApplicationTests.java
@@ -1,5 +1,12 @@
 package com.acosta.carros;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.acosta.carros.domain.Carro;
+import com.acosta.carros.domain.dto.CarroDTO;
 import com.acosta.carros.service.CarroService;
 
 @RunWith(SpringRunner.class)
@@ -19,10 +27,32 @@ public class CarrosApiApplicationTests {
 	@Test
 	public void test1() {
 		Carro carro = new Carro();
-		carro.setNome("ferrari");
+		carro.setNome("Ferrari");
 		carro.setTipo("esportivos");
 		
-		service.insert(carro);
+		CarroDTO c = service.insert(carro);
+		
+		assertNotNull(c);
+		
+		Long id = c.getId();
+		assertNotNull(id);
+		
+		//Buscar o objeto
+		Optional<CarroDTO>op = service.getCarroById(id);
+		assertTrue(op.isPresent());
+		
+		c = op.get();
+		assertEquals( "Ferrari",c.getNome());
+		assertEquals( "esportivos",c.getTipo());
+		
+		// Deletar o objeto
+		service.delete(id);
+		
+		// verificar se deletou
+		assertFalse(service.getCarroById(id).isPresent());
+		
+		
+		
 	}
 
 	@Test
